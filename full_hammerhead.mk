daaa975648ccbfe789fc8260f06fd8e08bcebe3e
GeyerA <calo.geyer@gmail.com>
2014-07-07 23:39:29 +0200

    Added Lightbulb

--- full_hammerhead.mk
+++ full_hammerhead.mk
@@ -30,5 +30,8 @@ PRODUCT_MODEL := AOSP on HammerHead
 PRODUCT_MANUFACTURER := LGE
 PRODUCT_RESTRICT_VENDOR_FILES := true
 
+ PRODUCT_PACKAGES += \
+    Lightbulb
+
 $(call inherit-product, device/lge/hammerhead/device.mk)
 $(call inherit-product-if-exists, vendor/lge/hammerhead/device-vendor.mk)
